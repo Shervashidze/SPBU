c8cf0ea8d5d1aaaea191bdd9e0975120d555ba4c
Shervashidze <gshervashidze1999@yandex.ru>
2017-11-16 00:49:40 +0300

    adding 4.3

---
+++ 4 homework/3/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "stack.h"
+using namespace std;
+
+const int maxSizeOfExpression = 1000;
+
+bool isHigher(Stack *stack, char statement)
+{
+    if (peek(stack) == '*' || peek(stack) == '/')
+        return true;
+    else
+        return ((statement == '+' || statement == '-') && peek(stack) != '(');
+}
+
+void processCharacter(Stack *stack, char term, char output[], int &outputPosition)
+{
+    if (term == '(')
+        push(stack, term);
+    else if (term == '+'|| term == '-' || term == '*' || term == '/')
+    {
+        while (!isEmpty(stack) && isHigher(stack, term))
+        {
+            output[outputPosition] = pop(stack);
+            outputPosition++;
+        }
+        push(stack, term);
+    }
+    else if (term == ')')
+    {
+        while (peek(stack) != '(')
+        {
+            output[outputPosition] = pop(stack);
+            outputPosition++;
+        }
+        pop(stack);
+    }
+    else if ((term - '0') >= 0 && (term - '0') <= 9)
+    {
+        output[outputPosition] = term;
+        outputPosition++;
+    }
+}
+
+int main()
+{
+    cout << "Enter expression: ";
+    Stack *stack = createStack();
+    char input[maxSizeOfExpression] = " ";
+    char output[maxSizeOfExpression] = " ";
+    cin.get(input, maxSizeOfExpression);
+
+    int outputPosition = 0;
+    int inputPosition = 0;
+
+    while (input[inputPosition] != '\0')
+    {
+        processCharacter(stack, input[inputPosition], output, outputPosition);
+        inputPosition++;
+    }
+
+    while (!isEmpty(stack))
+    {
+        output[outputPosition] = pop(stack);
+        outputPosition++;
+    }
+
+    cout << "Reverse Polish notation: ";
+    for (int counter = 0; counter < outputPosition; counter++)
+    {
+        cout << output[counter] << ' ';
+    }
+
+    emptyMemory(stack);
+    delete stack;
+}
---
+++ 4 homework/3/stack.cpp
@@ -0,0 +1,63 @@
+#include "stack.h"
+
+struct StackElement
+{
+    char value;
+    StackElement *next;
+};
+
+struct Stack
+{
+    StackElement *head;
+};
+
+bool isEmpty(Stack *stack)
+{
+    return (stack->head == nullptr);
+}
+
+Stack *createStack()
+{
+    Stack *newStack = new Stack;
+    newStack->head = nullptr;
+    return newStack;
+}
+
+void push(Stack *stack, char value)
+{
+    StackElement *newStack = new StackElement;
+    newStack->value = value;
+    newStack->next = stack->head;
+    stack->head = newStack;
+}
+
+char pop(Stack *stack)
+{
+   if (isEmpty(stack))
+   {
+       return -17;
+   }
+   StackElement *temp = new StackElement;
+   temp = stack->head;
+   char value = temp->value;
+
+   stack->head = stack->head->next;
+   delete temp;
+   temp = nullptr;
+   return value;
+}
+
+void emptyMemory(Stack *stack)
+{
+    while (!isEmpty(stack))
+    {
+        pop(stack);
+    }
+}
+
+char peek(Stack *stack)
+{
+    if (isEmpty(stack))
+        return 'k';
+    return stack->head->value;
+}
