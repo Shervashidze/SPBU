e887e9289016b869d3086a2e823f28c0d836a621
Shervashidze <gshervashidze1999@yandex.ru>
2017-11-23 21:23:18 +0300

    adding 7.1

---
+++ 7 homework/1/bst.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct BSTNode;
+
+struct BinarySearchTree
+{
+    BSTNode *root;
+};
+
+BinarySearchTree *createTree();
+void clearTree(BinarySearchTree *tree);
+
+bool contains(BinarySearchTree *tree, int value);
+bool add(BinarySearchTree *tree, int value);
+bool remove(BinarySearchTree *tree, int value);
+
+void print(BinarySearchTree *tree);
+void descendingPrint(BinarySearchTree *tree);
+void errorSearchPrint(BinarySearchTree *tree);
