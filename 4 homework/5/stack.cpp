9ddf381674d59e29138e8250afc9fde01e377b35
Shervashidze <33333462+Shervashidze@users.noreply.github.com>
2017-11-26 21:26:19 +0300

    Update stack.cpp

--- 4 homework/5/stack.cpp
+++ 4 homework/5/stack.cpp
@@ -37,7 +37,7 @@ char pop(Stack *stack)
    {
        return 'k';
    }
-   StackElement *temp = new StackElement;
+   StackElement *temp = nullptr;
    temp = stack->head;
    char value = temp->value;
 
