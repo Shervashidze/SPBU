0edaaaf4526b8e7b728ad79f803525953681d923
Shervashidze <gshervashidze1999@yandex.ru>
2017-12-17 22:25:25 +0300

    updating 5.3

--- 5 homework/3.cpp
+++ 5 homework/3.cpp
@@ -35,7 +35,6 @@ void isFirstMinus(int array[], char lowerString[], char upperString[], int &inde
         index++;
         array[0] = -array[0];
     }
-
 }
 
 void addChars(char upperString[], char lowerString[], char first, char second, int &index)
@@ -43,6 +42,22 @@ void addChars(char upperString[], char lowerString[], char first, char second, i
     upperString[index] = first;
     lowerString[index] = second;
     index++;
+}
+
+void addInString(char *upperString, char *lowerString, int number, int &index, int length, int i)
+{
+    int *digits = toDigits(number);
+    int counter = 0;
+    while (digits[counter] != -1)
+    {
+        if (digits[counter] == 1 && digits[counter + 1] == -1 && counter == 0 && i != length - 1)
+           break;
+        else
+            addChars(upperString, lowerString, ' ', digits[counter] + '0', index);
+        counter++;
+    }
+    delete[] digits;
+    digits = nullptr;
 }
 
 void processIndex(int i, int array[], char upperString[], char lowerString[], int &index, int &power, int length)
@@ -59,38 +74,12 @@ void processIndex(int i, int array[], char upperString[], char lowerString[], in
         array[i] = -array[i];
     }
 
-    int *digits = toDigits(array[i]);
-    int counter = 0;
-    while (digits[counter] != -1)
-    {
-        if (digits[counter] == 1 && digits[counter + 1] == -1 && counter == 0 && i != length - 1)
-           break;
-        else
-            addChars(upperString, lowerString, ' ', digits[counter] + '0', index);
-
-        counter++;
-    }
+    addInString(upperString, lowerString, array[i], index, length, i);
     addChars(upperString, lowerString, ' ', 'x', index);
-    delete[] digits;
-    digits = nullptr;
 
-    digits = toDigits(power);
-    counter = 0;
-    while (digits[counter] != -1)
-    {
-        if (digits[counter] == 1 && counter == 0 && digits[counter + 1] == -1)
-        {
-            addChars(upperString, lowerString, ' ', ' ', index);
-            break;
-        }
+    addInString(lowerString, upperString, power, index, length, i);
 
-        addChars(upperString, lowerString, digits[counter] + '0', ' ', index);
-        counter++;
-    }
     power--;
-    delete[] digits;
-    digits = nullptr;
-
     addChars(upperString, lowerString, ' ', '+', index);
     addChars(upperString, lowerString, ' ', ' ', index);
 }
@@ -105,14 +94,13 @@ void printPolynomial(int array[], int length)
     isFirstMinus(array, lowerString, upperString, index);
 
     for (int i = 0; i < length; i++)
-    {
         processIndex(i, array, upperString, lowerString, index, power, length);
-    }
 
     if (array[length - 1] != 0)
         lowerString[index - 3] = ' ';
 
-    lowerString[index - 2] = ' ';
+    lowerString[index - 2] = '\0';
+    lowerString[index - 2] = '\0';
     cout << upperString << '\n';
     cout << lowerString << '\n';
 }
