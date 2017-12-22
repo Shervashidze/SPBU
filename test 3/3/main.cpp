694c2caca12d9acf8841fbdf718e77efebad159e
Shervashidze <gshervashidze1999@yandex.ru>
2017-12-22 15:03:01 +0300

    adding test 3 third task

---
+++ test 3/3/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include "stack.h"
+
+using namespace std;
+const int maxWordSize = 10000;
+
+bool *initUsed(int n)
+{
+    bool *used = new bool[n];
+    for (int i = 0; i < n; i++)
+        used[i] = false;
+    used[0] = true;
+
+    return used;
+}
+
+void printBFS(int **matrix, int length)
+{
+    bool *used = initUsed(length);
+
+    Stack *stack = createStack();
+    push(stack, 0);
+    cout << "1 ";
+
+    while (!isEmpty(stack))
+    {
+        int vertex = pop(stack);
+        for (int i = 0; i < length; i++)
+        {
+            if (!used[i] && matrix[vertex][i] > 0)
+            {
+                used[i] = true;
+                push(stack, i);
+                cout << i + 1 << ' ';
+            }
+        }
+    }
+
+    emptyMemory(stack);
+}
+
+int main()
+{
+    char fileName[maxWordSize];
+    cout << "Enter file name: ";
+    cin >> fileName;
+
+    ifstream file(fileName);
+
+    if (!file.is_open())
+    {
+        cout << "File not found.";
+        int c = 0;
+        cin >> c;
+        return 0;
+    }
+
+    int length = 0;
+    file >> length;
+
+    int **matrix = new int*[length];
+    for (int i = 0; i < length; i++)
+        matrix[i] = new int[length];
+
+    for (int i = 0; i < length; i++)
+        for (int j = 0; j < length; j++)
+           file >> matrix[i][j];
+
+    printBFS(matrix, length);
+
+    for (int i = 0; i < length; i++)
+        delete[] matrix[i];
+    delete[] matrix;
+}
