7415dc86bc15f178103d62b67ee146daac6ceeef
Shervashidze <gshervashidze1999@yandex.ru>
2018-06-11 05:09:30 +0300

    fixing problems

--- sem 2/5 hw/3/src/main/java/group144/shervashidze/Calculator.java
+++ sem 2/5 hw/3/src/main/java/group144/shervashidze/Calculator.java
@@ -6,9 +6,17 @@ package group144.shervashidze;
  * When we are adding new operation, updating last value.
  */
 public class Calculator {
-    private double current;
+    /* tail is buffer value - calculation of previous operations and values. */
+    private double tail;
+    /* true if calculator was not initialized */
     private boolean isClear = true;
+    /* operation - last operation, current value of calculator is tail (operation) firstPriorityValue.
+    Can be only add or sub. */
     private Commands operation;
+    /* storing here last Value in case there would be mul or div as next operation */
+    private double firstPriorityValue;
+    /* storing here recent operation. Can be changed outside */
+    private Commands firstPriorityOp;
 
     /**
      * is Clear.
@@ -20,18 +28,26 @@ public class Calculator {
 
     /**
      * getValue
+     * current value is tail (operation) firstPriorityValue.
      * @return current value of the Calculator.
      */
     public double getValue() {
-        return current;
+        switch (operation) {
+            case ADDITION:
+                return tail + firstPriorityValue;
+            case SUBTRACTION:
+                return tail - firstPriorityValue;
+        }
+
+        return 42;
     }
 
     /**
      * SetOperation.
-     * @param op to set current operation.
+     * @param op to set first priority operation.
      */
     public void setOperation(Commands op) {
-        operation = op;
+        firstPriorityOp = op;
     }
 
     /**
@@ -40,34 +56,49 @@ public class Calculator {
      * @param op - operation to initialize calculator with.
      */
     public void setFirst(String string, Commands op) {
-        current = Double.valueOf(string);
-        operation = op;
+        tail = 0;
+        firstPriorityValue = Double.valueOf(string);
+        operation = Commands.ADDITION;
+        firstPriorityOp = op;
         isClear = false;
     }
 
     /**
      * calculate
-     * updates current.
-     * @param op - setting op as new operation
-     * @param string - to set current = current (old)operation string(double value).
+     * updates tail if firstPriorityOp is add or sub, else only work with firstPriorityValue.
+     * @param op - setting op as new firstPriorityOp;
+     * @param string - if firstPriorityOp is add or sub set firstPriorityValue as string, otherwise do operation with
+     *               firstPriorityValue.
      */
     public void calculate(Commands op, String string) {
         Double second = Double.valueOf(string);
-        switch (operation) {
+        switch (firstPriorityOp) {
             case ADDITION:
-                current = current + second;
+                if (operation == Commands.ADDITION) {
+                    tail = tail + firstPriorityValue;
+                } else {
+                    tail = tail - firstPriorityValue;
+                }
+                firstPriorityValue = second;
+                operation = firstPriorityOp;
                 break;
             case SUBTRACTION:
-                current = current - second;
+                if (operation == Commands.ADDITION) {
+                    tail = tail + firstPriorityValue;
+                } else {
+                    tail = tail - firstPriorityValue;
+                }
+                firstPriorityValue = second;
+                operation = firstPriorityOp;
                 break;
             case DIVISION:
-                current = current / second;
+                firstPriorityValue = firstPriorityValue / second;
                 break;
             case MULTIPLICATION:
-                current = current * second;
+                firstPriorityValue = firstPriorityValue * second;
                 break;
         }
 
-        operation = op;
+        firstPriorityOp = op;
     }
 }
