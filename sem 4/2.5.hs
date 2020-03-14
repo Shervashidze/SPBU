d95aad76e30c53d3ed1a12525e99546abd357246
George Shervashidze <gshervashidze1999@yandex.ru>
2020-03-15 02:39:08 +0300

    new take

--- sem 4/2.5.hs
+++ sem 4/2.5.hs
@@ -1,6 +1,9 @@
 module SumOfThree where
 
+sumOfTwo :: [Int] -> [Int] -> [Int]
+sumOfTwo [] list = list
+sumOfTwo list [] = list
+sumOfTwo (x:list) (y:list') = (x + y) : sumOfTwo list list'
+
 sumOfThree :: [Int] -> [Int] -> [Int] -> [Int]
-sumOfThree [] [] [] = []
-sumOfThree first second third = [(if first == [] then 0 else head first) + (if second == [] then 0 else head second) + (if third == [] then 0 else head third)] 
-     ++ sumOfThree (if first == [] then [] else tail first) (if second == [] then [] else tail second) (if third == [] then [] else tail third)
+sumOfThree list list' list'' = sumOfTwo (sumOfTwo list list') list''
