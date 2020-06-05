76da57da51a75c1c50a491e3554022899f74fc83
George Shervashidze <gshervashidze1999@yandex.ru>
2020-06-05 14:06:56 +0300

    adding 4.1

---
+++ sem 4/4/1/task 1.hs
@@ -0,0 +1,6 @@
+filterEven list = length (filter (even) list)
+
+mapEven list = length list - sum (map (\x -> mod x 2) list)
+
+foldrEven :: [Integer] -> Integer
+foldrEven = foldr (\x acc -> if even x then (acc + 1) else acc) 0
